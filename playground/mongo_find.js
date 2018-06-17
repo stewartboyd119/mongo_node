7efa31773d6fde52e13dcbfcf153262c22a1186f
Stewart Boyd <stewartboyd1988@gmail.com>
2018-06-17 14:05:35 -0400

    added query for michael

--- playground/mongo_find.js
+++ playground/mongo_find.js
@@ -2,18 +2,21 @@ const {MongoClient, ObjectID} = require("mongodb");
 
 const connectionString = "mongodb://localhost:27017";
 const dbName = "TodoApp";
-const todosCollection = "Todos";
+const todosCollection = "Users";
 
 MongoClient.connect(connectionString, (err, client) => {
     if (err) {
         return console.log("Error connection", err);
     }
     var db = client.db(dbName);
-    var result = db.collection(todosCollection).find({}).toArray().then((res) => {
-        console.log(res);
-        client.close();
+    var result = db.collection(todosCollection).find({user: "Michael"}).toArray().then((res) => {
+        console.log(JSON.stringify(res, undefined, 2));
+        //client.close();
     }, (err) => {
         return console.log("Error on find", err);
-        client.close();
+        //client.close();
     });
+    db.collection(todosCollection).find({}).count().then((number) => {
+        console.log(`The count was ${number}`);
+    })
 })
