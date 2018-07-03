81329397af43c6bfe3dac16563107d2b86754de1
Stewart Boyd <stewartboyd1988@gmail.com>
2018-07-02 20:17:04 -0400

    added a get todos to server

--- server/server.js
+++ server/server.js
@@ -22,6 +22,11 @@ app.post("/todos", (req, res) => {
     })
 });
 
+app.get("/todos", (req, res) => {
+    Todo.find({}).then((docs) => res.send({docs}),
+        (err) => res.status(400).send(err));
+})
+
 app.listen(3000, () => {
     console.log("server started");
 
