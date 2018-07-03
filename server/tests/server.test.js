d60b50f2b2c021424df845b50567c0bb3feab9c8
Stewart Boyd <stewartboyd1988@gmail.com>
2018-07-02 20:47:45 -0400

    added test for get all docs

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -3,10 +3,19 @@ const request = require('supertest');
 const {app} = require('../server');
 const {Todo} = require('../models/todo');
 
+const seedTodos = [
+    {text: "todo1"},
+    {text: "todo2"},
+    {text: "todo3"},
+    {text: "todoj"},
+    {text: "todo5"},
+    {text: "todo6"}
+] 
 // empty mongo before each request
 beforeEach((done) => {
     Todo.remove({}).then((res) => done(),
                          (err) => done(err));
+    Todo.insertMany(seedTodos);
 });
 describe('POST /todos', () => {
     it('respond with json', (done) => {
@@ -26,7 +35,7 @@ describe('POST /todos', () => {
             Todo.find().then(
                 (todos) => {
                     expect(todos[todos.length - 1].text).toBe(text);
-                    expect(todos.length).toBe(1);
+                    expect(todos.length).toBe(7);
                     return done();
             }, 
                 (err) => done(err))
@@ -44,10 +53,31 @@ describe('POST /todos', () => {
                 return done(err);
             }
             Todo.find().then((todos) => {
-                expect(todos.length).toBe(0);
+                expect(todos.length).toBe(6);
                 done();
             }, (err) => done(err))
             .catch((reason) => done(reason));
         });
-    })
+    });
+});
+
+
+describe("GET /todos", () => {
+    it("Get all docs", (done) => {
+        request(app)
+        .get('/todos')
+        .expect(200)
+        .end((err, res) => {
+            if (err) {
+                return done(err);
+            }
+            Todo.find().then((todos) => {
+                expect(todos.length).toBe(6);
+                done();
+            }
+                ,
+                (err) => done(err))
+                .catch((reason) => done(reason));
+        });
+    });
 });
