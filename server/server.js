203e64524fdb54a87bf7443d73ca0e792a70c755
Stewart Boyd <stewartboyd1988@gmail.com>
2018-07-06 18:55:46 -0400

    addeded patch functionality

--- server/server.js
+++ server/server.js
@@ -1,3 +1,4 @@
+const _ = require('lodash');
 const express = require('express');
 const bodyParser = require('body-parser');
 const {ObjectID} = require('mongodb');
@@ -44,6 +45,26 @@ app.get("/todos/:id", (req, res) => {
         },
         (err) => res.status(400).send()
     ).catch((reason) => res.status(400).send(`Crazy error ${reason}`));
+})
+
+app.patch("/todos/:id", (req, res) => {
+    var id = req.params.id;
+    if (!ObjectID.isValid(id)) {
+        return res.status(404).send();
+    }
+    var body = _.pick(req.body, ['text', 'completed']);
+    if (_.isBoolean(body.completed) && body.completed) {
+        body.completedAt = new Date().getTime();
+    } else {
+        body.completed = false;
+        body.completedAt = null;
+    }
+    Todo.findByIdAndUpdate(id, {$set: body}, {new: true}).then((newTodo) => {
+        if (!newTodo) {
+            return res.status(404).send();
+        }
+        res.status(200).send({todo: newTodo});
+    }, (err) => res.status(404).send());
 })
 
 // DELETE /todos/:id
