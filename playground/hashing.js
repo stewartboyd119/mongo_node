6fc85fd177f511ac797301f4f9ca2a05ee46bbe3
Stewart Boyd <stewartboyd1988@gmail.com>
2018-09-11 17:57:44 -0400

    add bcrpyt and some testing code in playground

--- playground/hashing.js
+++ playground/hashing.js
@@ -1,14 +1,28 @@
-const {SHA256} = require('crypto-js');
-const jwt = require('jsonwebtoken');
+// const {SHA256} = require('crypto-js');
+// const jwt = require('jsonwebtoken');
 
-var salt = 'somesecret';
+// var salt = 'somesecret';
 
-var data = {
-    id: 4
-};
+// var data = {
+//     id: 4
+// };
 
-var token = jwt.sign(data, salt);
-console.log(token);
+// var token = jwt.sign(data, salt);
+// console.log(token);
 
-var decoded = jwt.verify(token, salt);
-console.log('decoded', decoded);
+// var decoded = jwt.verify(token, salt);
+// console.log('decoded', decoded);
+
+const bcrpyt = require('bcryptjs');
+var plainPassword = "abc123";
+
+bcrpyt.genSalt(10, (error, salt) => {
+    bcrpyt.hash(plainPassword, salt, (error2, hashedValue) => {
+        console.log(hashedValue);
+    })
+});
+
+var hashedPassword = '$2a$11$7KlzzIcrfJavmi6dgm7ia.2H9z0qW3IrZW9K3r3oYQd4sy5gsisuG';
+bcrpyt.compare(plainPassword, hashedPassword, (error, doesMatch) => {
+    console.log(`shit does match = ${doesMatch}`);
+})
